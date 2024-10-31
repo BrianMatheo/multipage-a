5694dac984bf0813cbaafc861df82192aafc3d85
Brian Matheo <bmalvarezp@ufpso.edu.co>
2024-10-31 09:48:43 -0500

    implementacion de fuentes y posición sticky

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
-			  }
+				myfont: ['myfont', ...defaultTheme.fontFamily.sans],
+			  },
 		},
 	},
 	plugins: [],
